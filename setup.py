672680f61af98e001208980c38f851c5e2de2b41
hack3d <hack3d.dev@googlemail.com>
2018-01-27 20:21:45 +0100

    Update dependencies Set version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='stockanalyses-importer',
-	version='0.1.0',
+	version='0.1.1',
 	description='Import bitcoin and stock data.',
 	url='http://stockdashboard.de',
 	author='Raphael Lekies',
